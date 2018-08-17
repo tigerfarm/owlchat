9799b5e602e96596d018249f29253a8d0f78f548
tigerfarm <tigerfarm@gmail.com>
2018-08-17 14:08:24 -0700

    updates

--- custom/pageTop.js
+++ custom/pageTop.js
@@ -1,6 +1,15 @@
 document.write('<div id="topBar">');
 document.write('<table><tr>');
-document.write('<td><a href="/index.html"><img src="custom/companyLogo.jpg" alt="Twilio" style="width:160px; padding-right: 30px;"/></a></td>');
+document.write('<td><a href="/index.html"><img src="custom/companyLogo.jpg" alt="Twilio" style="width:120px; padding-right: 30px;"/></a></td>');
 document.write('<td><h1>Owl Chat</h1></td>');
+
+document.write('<td>');
+document.write('<div class="menuicon" onclick="menuicon()" style="margin: 9px 0 0 300px;">');
+document.write('    <div class="bar1"></div>');
+document.write('    <div class="bar2"></div>');
+document.write('    <div class="bar3"></div>');
+document.write('</div>');
+document.write('</td>');
+
 document.write('</tr></table>');
 document.write('</div>');
